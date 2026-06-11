6dd1c482d1257952874e42d7c2dfa177187777bd
HasnainAnsari1 <hasnain.ansari.git@gmail.com>
2026-06-11 12:38:53 +0500

    JSX and Components

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+// JSX In React And Js In JSX
+
+// import React from 'react'
+
+
+// function App() {
+//   const name = "raza"
+//   const age = 20
+
+//   return (
+//     <div>
+//       <h1>Student name is {name}</h1>
+//       <h2>Student age is {age}</h2>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// Simple functional Component in React
+// CREATED A FOLDER IN SRC NAME {COMPONENTS} IN COMPONENT FOLDER CREATED A USERCARDS.JSX FILE 
+// IN USERCARD.JSX WE CREATED A SIMPLE UI AND IMPORT IT IN APP FILE TO USE 
+// NOTE ** THIS IS WITHOUT PROPS DRILLING
+
+// import React from 'react'
+// import Usercards from './components/Usercards'
+
+// function App() {
+//   return (
+//     <div style={{display:"flex"}}>
+//       <Usercards name="hassan" role="student" status="online"/>
+//       <Usercards name="faizan" role="teacher" status="online"/>
+//       <Usercards name="saim" role="worker" status="offline"/>
+
+//     </div>
+//   )
+// }
+
+// export default App
---
+++ src/components/Usercards.jsx
@@ -0,0 +1,15 @@
+// Basic Usercard UI TO RE-USE
+
+// import React from 'react'
+
+// function Usercards({name,role,status}) {
+//   return (
+//     <div style={{width:"120px",backgroundColor:"lightgray", borderRadius:"7px", margin:"20px", padding:"5px"}}>
+//       <h1 style={{fontFamily:"JetBrains Mono, 'Courier New', monospace" }}>{name}</h1>
+//       <p>{role}</p>
+//       <h3>{status}</h3>
+//     </div>
+//   )
+// }
+
+// export default Usercards
